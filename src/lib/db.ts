c917285b483d55c1fae16bd55c33fc1e2e26e936
YayBlaze <blazeannison@icloud.com>
2025-12-08 17:07:34 -0800

    feat: added server code for login logout and creating accounts

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+export async function dbQuery(sql: string, args?: any[]): Promise<any> {}
+
+export async function login(usr: string, pass: string) {
+	return { auth: false, validUsr: false };
+}
+
+export async function hash(input: string): Promise<string> {
+	const data = new TextEncoder().encode(input);
+	const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+	const hashArray = Array.from(new Uint8Array(hashBuffer));
+	return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+}
